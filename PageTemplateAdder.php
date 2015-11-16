f9770dcc5c32db518cba1e5820a2cba2b6ba7632
Paul Craig <paul_craig_16@hotmail.com>
2015-11-16 18:33:44 +0000

    Reindent everything because I'm nuts.

--- PageTemplateAdder.php
+++ PageTemplateAdder.php
@@ -1,177 +1,172 @@
 <?php
 /**
- * HEAVILY INFLUENCED BY 
+ * HEAVILY INFLUENCED BY
  * @see http://www.wpexplorer.com/wordpress-page-templates-plugin/
  */
 class PageTemplateAdder {
 
-	/**
-         * A Unique Identifier
-         */
-	protected $plugin_slug;
+    /**
+    * A Unique Identifier
+    */
+    protected $plugin_slug;
 
-        /**
-         * A reference to an instance of this class.
-         */
-        private static $instance;
+    /**
+    * A reference to an instance of this class.
+    */
+    private static $instance;
 
-        /**
-         * The array of templates that this plugin tracks.
-         */
-        protected $templates;
-
-
-        /**
-         * Returns an instance of this class. 
-         */
-        public static function get_instance() {
-
-                if( null == self::$instance ) {
-                        self::$instance = new PageTemplateAdder();
-                } 
-
-                return self::$instance;
-
-        } 
-
-        /**
-         * Initializes the plugin by setting filters and administration functions.
-         */
-        private function __construct() {
-
-                $this->plugin_slug = 'zw-ch-cv';
-
-                $this->templates = array();
+    /**
+    * The array of templates that this plugin tracks.
+    */
+    protected $templates;
 
 
-                // Add a filter to the attributes metabox to inject template into the cache.
-                add_filter(
-					'page_attributes_dropdown_pages_args',
-					 array( $this, 'register_project_templates' ) 
-				);
+    /**
+    * Returns an instance of this class.
+    */
+    public static function get_instance() {
 
-
-                // Add a filter to the save post to inject out template into the page cache
-                add_filter(
-					'wp_insert_post_data', 
-					array( $this, 'register_project_templates' ) 
-				);
-
-
-                // Add a filter to the template include to determine if the page has our 
-				// template assigned and return it's path
-                add_filter(
-					'template_include', 
-					array( $this, 'view_project_template') 
-				);
-
-                // Add an action to enqueue styles based on if they exist in the 
-                // /css folder with the same name as the template
-                add_action( 'wp_enqueue_scripts', 
-                        array( $this, 'enqueue_styles') );
-
-
-
-                // Add your templates to this array.
-                $this->templates = array(
-                        'page-cv.php'     => 'Paul\'s CV',
-                );	
+        if( null == self::$instance ) {
+            self::$instance = new PageTemplateAdder();
         }
 
+        return self::$instance;
+    }
 
-        /**
-         * Foreach template, cylce through their filenames and look for corresponding css files with the same filenames.
-         * 
-         * @return [type] [description]
-         */
-        public function enqueue_styles() {
+    /**
+    * Initializes the plugin by setting filters and administration functions.
+    */
+    private function __construct() {
 
-                if( ! empty( $this->templates ) ) {
+        $this->plugin_slug = 'zw-ch-cv';
 
-                        $template_filenames = array_keys( $this->templates );
+        $this->templates = array();
 
-                        foreach( $template_filenames as $template_filename ) {
 
-                                //grab the filename of the template, sans the  '.php' suffix
-                                $filename = basename( $template_filename, '.php' );
+        // Add a filter to the attributes metabox to inject template into the cache.
+        add_filter(
+            'page_attributes_dropdown_pages_args',
+            array( $this, 'register_project_templates' )
+        );
 
-                                //this is a good idea, except our dev theme is fucked.
-                                //if( file_exists( plugins_url( '/css/' . $filename . '.css', __FILE__ ) ) ) {
-                                                        
-                                        wp_enqueue_style( $this->plugin_slug . '-' . $filename,
-                                                plugins_url( '/css/' . $filename . '.css', __FILE__ ), 
-                                                array());
-                                //}
-                        }
-                }    
+
+        // Add a filter to the save post to inject out template into the page cache
+        add_filter(
+            'wp_insert_post_data',
+            array( $this, 'register_project_templates' )
+        );
+
+
+        // Add a filter to the template include to determine if the page has our
+        // template assigned and return it's path
+        add_filter(
+            'template_include',
+            array( $this, 'view_project_template')
+        );
+
+
+        // Add an action to enqueue styles based on if they exist in the
+        // /css folder with the same name as the template
+        add_action( 'wp_enqueue_scripts',
+        array( $this, 'enqueue_styles') );
+
+
+        // Add your templates to this array.
+        $this->templates = array(
+            'page-cv.php'     => 'Paul\'s CV',
+        );
+    }
+
+
+    /**
+    * Foreach template, cylce through their filenames and look for corresponding css files with the same filenames.
+    *
+    * @return [type] [description]
+    */
+    public function enqueue_styles() {
+
+        if (!empty($this->templates)) {
+
+            $template_filenames = array_keys($this->templates);
+
+            foreach ($template_filenames as $template_filename) {
+
+                //grab the filename of the template, sans the  '.php' suffix
+                $filename = basename($template_filename, '.php');
+
+                //this is a good idea, except our dev theme is fucked.
+                //if( file_exists( plugins_url( '/css/' . $filename . '.css', __FILE__ ) ) ) {
+
+                wp_enqueue_style($this->plugin_slug . '-' . $filename,
+                    plugins_url('/css/' . $filename . '.css', __FILE__),
+                    array());
+                ///}
+            }
+        }
+    }
+
+    /**
+    * Adds our template to the pages cache in order to trick WordPress
+    * into thinking the template file exists where it doesn't really exist.
+    */
+    public function register_project_templates( $atts ) {
+
+        // Create the key used for the themes cache
+        $cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
+
+        // Retrieve the cache list.
+        // If it doesn't exist, or it's empty prepare an array
+        $templates = wp_get_theme()->get_page_templates();
+        if ( empty( $templates ) ) {
+            $templates = array();
         }
 
-        /**
-         * Adds our template to the pages cache in order to trick WordPress
-         * into thinking the template file exists where it doens't really exist.
-         *
-         */
-        public function register_project_templates( $atts ) {
+        // New cache, therefore remove the old one
+        wp_cache_delete( $cache_key , 'themes');
 
-                // Create the key used for the themes cache
-                $cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
+        // Now add our template to the list of templates by merging our templates
+        // with the existing templates array from the cache.
+        $templates = array_merge( $templates, $this->templates );
 
-                // Retrieve the cache list. 
-				// If it doesn't exist, or it's empty prepare an array
-                $templates = wp_get_theme()->get_page_templates();
-                if ( empty( $templates ) ) {
-                        $templates = array();
-                } 
+        // Add the modified cache to allow WordPress to pick it up for listing
+        // available templates
+        wp_cache_add( $cache_key, $templates, 'themes', 1800 );
 
-                // New cache, therefore remove the old one
-                wp_cache_delete( $cache_key , 'themes');
+        return $atts;
+    }
 
-                // Now add our template to the list of templates by merging our templates
-                // with the existing templates array from the cache.
-                $templates = array_merge( $templates, $this->templates );
+    /**
+    * Checks if the template is assigned to the page
+    */
+    public function view_project_template( $template ) {
 
-                // Add the modified cache to allow WordPress to pick it up for listing
-                // available templates
-                wp_cache_add( $cache_key, $templates, 'themes', 1800 );
+        global $post;
 
-                return $atts;
-        } 
+        /* On search pages, for example, there may be no $post object */
+        if( is_null( $post ) ) {
 
-        /**
-         * Checks if the template is assigned to the page
-         */
-        public function view_project_template( $template ) {
+            return $template;
+        }
 
-                global $post;
+        if (!isset($this->templates[get_post_meta(
+            $post->ID, '_wp_page_template', true
+        )] ) ) {
 
-                /* On search pages, for example, there may be no $post object */
-                if( is_null( $post ) ) {
+            return $template;
+        }
 
-                        return $template;
-                }
+        $file = plugin_dir_path(__FILE__). get_post_meta(
+            $post->ID, '_wp_page_template', true
+        );
 
-                if (!isset($this->templates[get_post_meta( 
-					$post->ID, '_wp_page_template', true 
-				)] ) ) {
-					
-                        return $template;
-						
-                } 
+        // Just to be safe, we check if the file exist first
+        if( file_exists( $file ) ) {
+            return $file;
+        }
+        else { echo $file; }
 
-                $file = plugin_dir_path(__FILE__). get_post_meta( 
-					$post->ID, '_wp_page_template', true 
-				);
-				
-                // Just to be safe, we check if the file exist first
-                if( file_exists( $file ) ) {
-                        return $file;
-                } 
-				else { echo $file; }
-
-                return $template;
-        } 
-
-
-} 
+        return $template;
+    }
+}
 
 add_action( 'plugins_loaded', array( 'PageTemplateAdder', 'get_instance' ) );
--- page-cv.php
+++ page-cv.php
@@ -7,172 +7,172 @@ Description: A Page Template that is my resume.
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head profile="http://gmpg.org/xfn/11">
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><?php wp_title(''); ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?php wp_title(''); ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
 </head>
 <body <?php body_class();?> >
-<div class="cv__wrapper">
-	<header class="cv__header">
-		<h1 class="cv__name cv__title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Paul Craig</a></h1>
-		<h2 class="cv__description color__red">writes good code that does good</h2>
-		<div class="clearer"></div>
-	</header>
-	<!-- end of .cv__header -->
-	<div class="cv">
-		<div class="layout">
-			<div class="layout__item two-thirds palm-one-whole">
-				<div class="layout col_1">
-					<div class="cell cell--col_1">
-						<div class="layout__item one-whole">
-							<h3 class="section__title">work</h3>
-						</div>
-						<article class="job_description">
-							<div class="layout__item one-fifth">
-								<p class="job_description__year">2015</p>
-							</div><!--
-							--><div class="layout__item four-fifths">
-								<h5 class="job_description__title">Junior Developer</h5>
-								<h6 class="job_description__employer"><a class="pseudo_href" href="https://digitalmarketplace.blog.gov.uk">Government Digital Service</a></h6>
-								<p>Fixing public-sector procurement without trailing semicolons</p>
-                                <p>Submit public, well-documented, well-tested pull requests</p>
-                                <p>Receive code reviews; review others' code</p>
-                                <p>Effective member of a focused, collaborative team</p>
-							</div>
-						</article>
-						<article class="job_description">
-							<div class="layout__item one-fifth">
-								<p class="job_description__year">2014</p>
-							</div><!--
-							--><div class="layout__item four-fifths">
-								<h5 class="job_description__title">Web Developer</h5>
-								<h6 class="job_description__employer"><a class="pseudo_href" href="http://westernusc.ca">Western University Students’ Council</a></h6>
-								<p>Revitalized the main website of the students' council (USC)</p>
-								<p>Prioritized needs based on findings of user research</p>
-								<p>Collaborated with open-source developers where necessary</p>
-								<p>Incorporated third-party APIs; introduced a USC API</p>
-							</div>
-						</article>
-						<article class="job_description">
-							<div class="layout__item one-fifth">
-								<p class="job_description__year">2013-<span class="show_on_mobile">20</span>14</p>
-							</div><!--
-							--><div class="layout__item four-fifths">
-								<h5 class="job_description__title">Technical Infrastructure Intern</h5>
-								<h6 class="job_description__employer"><a class="pseudo_href" href="http://westernusc.ca">Western University Students’ Council</a></h6>
-                                <p>Worked on web-centric projects for the student government</p>
-                                <p>Installed, configured, and customised small websites</p>
-								<p>Wrote a <a href="http://testwestern.ca">web application</a> meant to pair students with clubs</p>
-								<p>Researched (and advised against) commissioning an app</p>
-							</div>
-						</article>
-					</div>
-					<div class="cell cell--col_1">
-						<div class="layout__item one-whole">
-							<h3 class="section__title">school</h3>
-						</div>
-						<article class="job_description">
-							<div class="layout__item one-fifth"></div><!--
-							--><div class="layout__item four-fifths">
-								<h5 class="job_description__title"><a href="http://www.csd.uwo.ca">Computer Science</a></h5>
-								<p>Studied foundational data structures and algorithms</p>
-								<p>Worked frequently on team-based semester-long projects</p>
-								<p>Learned about storing plaintext passwords in a database</p>
-							</div>
-						</article>
-						<article class="job_description">
-							<div class="layout__item one-fifth"></div><!--
-							--><div class="layout__item four-fifths">
-								<h5 class="job_description__title"><a href="http://www.fims.uwo.ca">Media, Information and Technoculture (MIT)</a></h5>
-								<p>Learned to communicate professionally without effacing myself</p>
-								<p>Critically analyzed nearly bloody everything</p>
-							</div>
-						</article>
-					</div>
-				</div><!-- end of .col_1 -->
-			</div><!-- end of .two-thirds
-			--><div class="layout__item one-third palm-one-whole">
-                <div class="layout col_2">
-                    <div class="cell cell--col_2">
-                        <div class="layout__item one-whole">
-							<h3 class="section__title color__red">contact</h3>
-						</div>
-						<article class="job_description">
-                            <div class="layout__item two-fifths palm-one-fifth"></div><!--
-							--><div class="layout__item three-fifths palm-four-fifths">
-								<a class="contact--body contact--email color__red" href="mailto:paul@pcraig3.ca?subject=Hello%20Paul!">paul&commat;pcraig3.ca</a>
-								<a class="contact--body" href="tel:+447421742811">(&plus;44) 7421742811</a>
-								<a class="contact--body" href="https://twitter.com/pcraig3">&commat;pcraig3</a>
-							</div>
-                        </article>
-					</div>
-                    <div class="cell cell--col_2">
-                        <div class="layout__item one-whole">
-							<h3 class="section__title">degree</h3>
-						</div>
-						<article class="job_description">
-							<div class="layout__item two-fifths palm-one-fifth">
-								<p class="job_description__year">2014</p>
-							</div><!--
-							--><div class="layout__item three-fifths palm-four-fifths">
-								<h5 class="job_description__title">Computer Science &amp; MIT Double Major</h5>
-								<p><a href="http://www.uwo.ca/">Western University</a></p>
-								<p>London, Canada</p>
-							</div>
-						</article>
-					</div>
-					<div class="cell cell--col_2">
-                        <div class="layout__item one-whole">
-							<h3 class="section__title">familiar with</h3>
-						</div>
-						<article class="job_description">
-							<div class="layout__item palm-one-fifth show_on_mobile"></div><!--
-							--><div class="layout__item two-fifths palm-four-fifths">
-								<p>Python</p>
-								<p>WordPress</p>
-								<p>git / GitHub</p>
-								<p>Java</p>
-								<p>C / C&plus;&plus;</p>
-								<p class="not_on_mobile">etc.</p>
-							</div><!--
-							--><div class="layout__item palm-one-fifth show_on_mobile"></div><!--
-							--><div class="layout__item three-fifths palm-four-fifths">
-								<p>PHP</p>
-								<p>HTML (templating)</p>
-								<p>CSS / SCSS</p>
-								<p>JavaScript / jQuery</p>
-								<p>SQL / Doctrine</p>
-								<p class="show_on_mobile">etc.</p>
-							</div>
-						</article>
-					</div>
-					<div class="cell cell--col_2">
-                        <div class="layout__item one-whole">
-							<h3 class="section__title">able to</h3>
-						</div>
-						<article class="job_description">
-							<div class="layout__item palm-one-fifth show_on_mobile"></div><!--
-						--><div class="layout__item one-whole palm-four-fifths">
-								<p>construct / defend arguments</p>
-								<p>construct / maintain software</p>
-								<p>give presentations on either</p>
-								<p>identify technical / political problems</p>
-								<p>work well within a team</p>
-							</div>
-						</article>
-					</div>
-				</div><!-- end of .col_2 -->
-			</div><!-- end of .one-third -->
-		</div><!-- end of .layout -->
-	</div>
-	<!-- end of .cv -->
-	<footer class="cv__footer">
-		<span class="cv__references">References available on request</span>
-		<div class="clearer"></div>
-	</footer>
-	<!-- end of .cv__footer -->
+  <div class="cv__wrapper">
+    <header class="cv__header">
+      <h1 class="cv__name cv__title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Paul Craig</a></h1>
+      <h2 class="cv__description color__red">codes right, leans left</h2>
+      <div class="clearer"></div>
+    </header>
+    <!-- end of .cv__header -->
+    <div class="cv">
+      <div class="layout">
+        <div class="layout__item two-thirds palm-one-whole">
+          <div class="layout col_1">
+            <div class="cell cell--col_1">
+              <div class="layout__item one-whole">
+                <h3 class="section__title">work</h3>
+              </div>
+              <article class="job_description">
+                <div class="layout__item one-fifth">
+                  <p class="job_description__year">2015</p>
+              </div><!--
+            --><div class="layout__item four-fifths">
+            <h5 class="job_description__title">Junior Developer</h5>
+            <h6 class="job_description__employer"><a class="pseudo_href" href="https://digitalmarketplace.blog.gov.uk">Government Digital Service</a></h6>
+            <p>Fixing public-sector procurement without trailing semicolons</p>
+            <p>Submit public, well-documented, well-tested pull requests</p>
+            <p>Receive code reviews; review others' code</p>
+            <p>Effective member of a focused, collaborative team</p>
+          </div>
+        </article>
+        <article class="job_description">
+          <div class="layout__item one-fifth">
+            <p class="job_description__year">2014</p>
+              </div><!--
+            --><div class="layout__item four-fifths">
+            <h5 class="job_description__title">Web Developer</h5>
+            <h6 class="job_description__employer"><a class="pseudo_href" href="http://westernusc.ca">Western University Students’ Council</a></h6>
+            <p>Revitalized the main website of the students' council (USC)</p>
+            <p>Prioritized needs based on findings of user research</p>
+            <p>Collaborated with open-source developers where necessary</p>
+            <p>Incorporated third-party APIs; introduced a USC API</p>
+          </div>
+        </article>
+        <article class="job_description">
+          <div class="layout__item one-fifth">
+            <p class="job_description__year">2013-<span class="show_on_mobile">20</span>14</p>
+              </div><!--
+            --><div class="layout__item four-fifths">
+            <h5 class="job_description__title">Technical Infrastructure Intern</h5>
+            <h6 class="job_description__employer"><a class="pseudo_href" href="http://westernusc.ca">Western University Students’ Council</a></h6>
+            <p>Worked on web-centric projects for the student government</p>
+            <p>Installed, configured, and customised small websites</p>
+            <p>Wrote a <a href="http://testwestern.ca">web application</a> meant to pair students with clubs</p>
+            <p>Researched (and advised against) commissioning an app</p>
+          </div>
+        </article>
+      </div>
+      <div class="cell cell--col_1">
+        <div class="layout__item one-whole">
+          <h3 class="section__title">school</h3>
+        </div>
+        <article class="job_description">
+              <div class="layout__item one-fifth"></div><!--
+            --><div class="layout__item four-fifths">
+            <h5 class="job_description__title"><a href="http://www.csd.uwo.ca">Computer Science</a></h5>
+            <p>Studied foundational data structures and algorithms</p>
+            <p>Worked frequently on team-based semester-long projects</p>
+            <p>Learned about storing plaintext passwords in a database</p>
+          </div>
+        </article>
+        <article class="job_description">
+              <div class="layout__item one-fifth"></div><!--
+            --><div class="layout__item four-fifths">
+            <h5 class="job_description__title"><a href="http://www.fims.uwo.ca">Media, Information and Technoculture (MIT)</a></h5>
+            <p>Learned to communicate professionally without effacing myself</p>
+            <p>Critically analyzed nearly bloody everything</p>
+          </div>
+        </article>
+      </div>
+    </div><!-- end of .col_1 -->
+      </div><!-- end of .two-thirds
+    --><div class="layout__item one-third palm-one-whole">
+    <div class="layout col_2">
+      <div class="cell cell--col_2">
+        <div class="layout__item one-whole">
+          <h3 class="section__title color__red">contact</h3>
+        </div>
+        <article class="job_description">
+              <div class="layout__item two-fifths palm-one-fifth"></div><!--
+            --><div class="layout__item three-fifths palm-four-fifths">
+            <a class="contact--body contact--email color__red" href="mailto:paul@pcraig3.ca?subject=Hello%20Paul!">paul&commat;pcraig3.ca</a>
+            <a class="contact--body" href="tel:+447421742811">(&plus;44) 7421742811</a>
+            <a class="contact--body" href="https://twitter.com/pcraig3">&commat;pcraig3</a>
+          </div>
+        </article>
+      </div>
+      <div class="cell cell--col_2">
+        <div class="layout__item one-whole">
+          <h3 class="section__title">degree</h3>
+        </div>
+        <article class="job_description">
+          <div class="layout__item two-fifths palm-one-fifth">
+            <p class="job_description__year">2014</p>
+              </div><!--
+            --><div class="layout__item three-fifths palm-four-fifths">
+            <h5 class="job_description__title">Computer Science &amp; MIT Double Major</h5>
+            <p><a href="http://www.uwo.ca/">Western University</a></p>
+            <p>London, Canada</p>
+          </div>
+        </article>
+      </div>
+      <div class="cell cell--col_2">
+        <div class="layout__item one-whole">
+          <h3 class="section__title">familiar with</h3>
+        </div>
+        <article class="job_description">
+              <div class="layout__item palm-one-fifth show_on_mobile"></div><!--
+            --><div class="layout__item two-fifths palm-four-fifths">
+            <p>Python</p>
+            <p>WordPress</p>
+            <p>git / GitHub</p>
+            <p>Java</p>
+            <p>C / C&plus;&plus;</p>
+            <p class="not_on_mobile">etc.</p>
+              </div><!--
+              --><div class="layout__item palm-one-fifth show_on_mobile"></div><!--
+            --><div class="layout__item three-fifths palm-four-fifths">
+            <p>PHP</p>
+            <p>HTML (templating)</p>
+            <p>CSS / SCSS</p>
+            <p>JavaScript / jQuery</p>
+            <p>SQL / Doctrine</p>
+            <p class="show_on_mobile">etc.</p>
+          </div>
+        </article>
+      </div>
+      <div class="cell cell--col_2">
+        <div class="layout__item one-whole">
+          <h3 class="section__title">able to</h3>
+        </div>
+        <article class="job_description">
+              <div class="layout__item palm-one-fifth show_on_mobile"></div><!--
+            --><div class="layout__item one-whole palm-four-fifths">
+            <p>construct / defend arguments</p>
+            <p>construct / maintain software</p>
+            <p>give presentations on either</p>
+            <p>identify technical / political problems</p>
+            <p>work well within a team</p>
+          </div>
+        </article>
+      </div>
+    </div><!-- end of .col_2 -->
+  </div><!-- end of .one-third -->
+</div><!-- end of .layout -->
+</div>
+<!-- end of .cv -->
+<footer class="cv__footer">
+  <span class="cv__references">References available on request</span>
+  <div class="clearer"></div>
+</footer>
+<!-- end of .cv__footer -->
 </div>
 <!-- end of .cv__wrapper -->
 <?php wp_footer(); ?>
